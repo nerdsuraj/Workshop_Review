e8313c1c6cd889b502f27755691a12930186fb72
suraj <soorajkrpandit@gmail.com>
2022-05-18 10:36:14 +0530

    Add project files.

---
+++ Workshop_Review/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop_Review
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToeGame.Game();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Workshop_Review/TicTacToeGame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop_Review
+{
+     class TicTacToeGame
+    {
+        public static void Game()
+        {
+            char[] game = new char[10];
+            game[0] = ' ';
+            game[1] = ' ';
+            game[2] = ' ';
+            game[3] = ' ';
+            game[4] = ' ';
+            game[5] = ' ';
+            game[6] = ' ';
+            game[7] = ' ';
+            game[8] = ' ';
+            game[9] = ' ';
+
+         // for print whatever your value you given to char[] in one way
+
+            foreach (char c in game)
+            {
+                Console.WriteLine(c);
+            }
+            
+            
+        }
+    }
+}
